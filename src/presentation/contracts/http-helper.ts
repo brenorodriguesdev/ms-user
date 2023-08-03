5175be64261553c50e333dfed583c5aa25f971f2
Breno Rodrigues <brenorodriguesdev@gmail.com>
2023-08-03 00:03:29 -0300

    feat: add http helper

---
+++ src/presentation/contracts/http-helper.ts
@@ -0,0 +1,22 @@
+import { ServerError } from '../errors/server-error'
+import { type HttpResponse } from './http'
+
+export const serverError = (): HttpResponse<Error> => ({
+  data: new ServerError(),
+  statusCode: 500
+})
+
+export const badRequest = (error: Error): HttpResponse<Error> => ({
+  data: error,
+  statusCode: 400
+})
+
+export const ok = (data: any): HttpResponse<any> => ({
+  data,
+  statusCode: 200
+})
+
+export const created = (): HttpResponse<void> => ({
+  data: null,
+  statusCode: 201
+})

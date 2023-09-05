34c232a90a5abf3e9d83471f8e9ede011f640d83
Breno Rodrigues <brenorodriguesdev@gmail.com>
2023-09-04 22:12:11 -0300

    feat: sign in controller

---
+++ src/presentation/controllers/sign-in.ts
@@ -0,0 +1,26 @@
+import { type SignInModel, type SignInResultModel } from '../../domain/models/sign-in'
+import { type SignInUseCase } from '../../domain/use-cases/sign-in'
+import { type Validator } from '../../validation/contracts/validator'
+import { type Controller } from '../contracts/controller'
+import { type HttpRequest, type HttpResponse } from '../contracts/http'
+import { badRequest, ok, serverError } from '../contracts/http-helper'
+
+export class SignInController implements Controller<SignInModel, SignInResultModel | Error> {
+  constructor (
+    private readonly validator: Validator<SignInModel>,
+    private readonly signInUseCase: SignInUseCase
+  ) {}
+
+  async handle (httpRequest: HttpRequest<SignInModel>): Promise<HttpResponse<SignInResultModel | Error>> {
+    try {
+      const error = this.validator.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const signInResult = await this.signInUseCase.sign(httpRequest.body)
+      return ok(signInResult)
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
--- src/test/stubs/sign-in.ts
+++ src/test/stubs/sign-in.ts
@@ -2,7 +2,7 @@ import { type SignInResultModel } from '../../domain/models/sign-in'
 import { type SignInUseCase } from '../../domain/use-cases/sign-in'
 import { signInResultMock } from '../mocks/sign-in-result'
 
-export class SignInStub implements SignInUseCase {
+export class SignInUseCaseStub implements SignInUseCase {
   async sign (): Promise<SignInResultModel> {
     return signInResultMock
   }
